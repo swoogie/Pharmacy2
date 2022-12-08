0dc1131341b593281ef6b2bd76b0c9e70ee69c74
Ignas Bieksa <ignasbieksa@gmail.com>
2022-12-08 23:35:01 +0200

    view orders

--- Pharmacy2/Areas/Admin/Controllers/DrugsController.cs
+++ Pharmacy2/Areas/Admin/Controllers/DrugsController.cs
@@ -29,8 +29,21 @@ namespace Pharmacy2.Areas.Admin.Controllers
             ViewBag.PageRange = pageSize;
             ViewBag.TotalPages = (int)Math.Ceiling((decimal)_context.Drugs.Count() / pageSize);
 
-            return View(await _context.Drugs.OrderByDescending(p => p.Id)
-                .Include(p => p.Category)
+            return View(await _context.Drugs.OrderByDescending(d => d.Id)
+                .Include(d => d.Category)
+                .Skip((p - 1) * pageSize)
+                .Take(pageSize)
+                .ToListAsync());
+        }
+
+        public async Task<IActionResult> FetchOrders(int p = 1)
+        {
+            int pageSize = 3;
+            ViewBag.PageNumber = p;
+            ViewBag.PageRange = pageSize;
+            ViewBag.TotalPages = (int)Math.Ceiling((decimal)_context.Orders.Count() / pageSize);
+
+            return View(await _context.Orders.OrderByDescending(d => d.Id)
                 .Skip((p - 1) * pageSize)
                 .Take(pageSize)
                 .ToListAsync());
